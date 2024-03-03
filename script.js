b859497fb222b6d80db70a5f04f76e5bcf61cb86
jrme421 <jeromepchallet@gmail.com>
2024-03-03 09:44:53 +0900

    final commit

--- script.js
+++ script.js
@@ -23,7 +23,20 @@ name conversion working 😉
 § This challenge is difficult on purpose, so start watching the solution in case
 you're stuck. Then pause and continue!
 */
+
 document.body.append(document.createElement("textarea"));
 document.body.append(document.createElement("button"));
 
-const text = document.querySelector("textarea").value;
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  const rows = text.split("\n");
+
+  for (const [i, row] of rows.entries()) {
+    row.toLowerCase().trim().split("_");
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${(output, padEnd(20))}${"✅".repeat(i + 1)}`);
+  }
+});
